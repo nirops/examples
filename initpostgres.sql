0d9d2640d311cb7bc7140631e3e02542984835f5
vivekagate <vivekagate@gmail.com>
2021-12-19 00:51:22 -0800

    Adding sample table creation

--- initpostgres.sql
+++ initpostgres.sql
@@ -1,1 +1,13 @@
 create database test;
+\c test;
+CREATE TABLE student
+(
+    id bigint NOT NULL,
+    name text COLLATE pg_catalog."default",
+    CONSTRAINT student_pkey PRIMARY KEY (id)
+);
+
+INSERT INTO student(id, name) VALUES
+ (1, 'A'),
+ (2, 'B'),
+ (3, 'C');
